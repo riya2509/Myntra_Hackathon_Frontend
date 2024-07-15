a9127d5997baa37228ff16beacd7c168b9c16c62
riya2509 <91462077+riya2509@users.noreply.github.com>
2024-07-15 17:29:25 +0530

    Routing and adding condition for global header

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@
 
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import LandingPage from "./components/LandingPage";
 import Card from "./components/Card";
@@ -21,49 +21,50 @@ import Streak from "./components/Streak";
 import shoesImage from "./assets/shoes.png";
 import dressesImage from "./assets/dresses.png";
 import handbagsImage from "./assets/handbags.png";
+import BidFunc from "./BidFunc";
 
 function App() {
   const mockStreak = 19;
-
+  const location = useLocation();
+  console.log(location);
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route
-            path="/bidding"
-            element={
-              <>
-                <div className="streak-container">
-                  <Streak streak={mockStreak} />
-                </div>
-                <main className="main">
-                  <Card
-                    title="Shoes"
-                    time="10:00 am - 10:05 am"
-                    imageUrl={shoesImage}
-                  />
-                  <Card
-                    title="Dresses"
-                    time="01:00 pm - 01:05 pm"
-                    imageUrl={dressesImage}
-                  />
-                  <Card
-                    title="Handbags"
-                    time="05:00 pm - 05:05 pm"
-                    imageUrl={handbagsImage}
-                  />
-                </main>
-              </>
-            }
-          />
-          <Route path="/shoes" element={<div>Shoes Page</div>} />
-          <Route path="/dresses" element={<div>Dresses Page</div>} />
-          <Route path="/handbags" element={<div>Handbags Page</div>} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      {!location.pathname.includes("/auction") && <Header />}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route
+          path="/bidding"
+          element={
+            <>
+              <div className="streak-container">
+                <Streak streak={mockStreak} />
+              </div>
+              <main className="main">
+                <Card
+                  title="Shoes"
+                  time="10:00 am - 10:05 am"
+                  imageUrl={shoesImage}
+                  link={"/auction"}
+                />
+                <Card
+                  title="Dresses"
+                  time="01:00 pm - 01:05 pm"
+                  imageUrl={dressesImage}
+                />
+                <Card
+                  title="Handbags"
+                  time="05:00 pm - 05:05 pm"
+                  imageUrl={handbagsImage}
+                />
+              </main>
+            </>
+          }
+        />
+        <Route path="/auction" element={<BidFunc />} />
+        <Route path="/dresses" element={<div>Dresses Page</div>} />
+        <Route path="/handbags" element={<div>Handbags Page</div>} />
+      </Routes>
+    </div>
   );
 }
 
